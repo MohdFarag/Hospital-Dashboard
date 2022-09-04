2966e957ff4070f98e96637fff918d80b8fa7925
MohdFarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2022-09-04 14:18:38 +0300

    delete unused photos

--- Server/database.py
+++ Server/database.py
@@ -8,7 +8,8 @@ def mysql_connector():
     user="root",
     passwd="01140345493",
     database="almazadb"
-)
+    )
+    print("Connected to database..")
 
     # Initialize our cursor
     mycursor = mydb.cursor(buffered=True)
@@ -16,7 +17,67 @@ def mysql_connector():
     return mydb, mycursor
 
 
+"""Retrive Database Tables"""
+
+def admin(mycursor) :
+    'Retrieve all admin data'
+    mycursor.execute("SELECT * FROM admin")
+    table = mycursor.fetchall()
+
+    return table
+
+def device(mycursor) :
+    'Retrieve all device data'
+    mycursor.execute("SELECT * FROM device")
+    table = mycursor.fetchall()
+
+    return table
+
+def equipment(mycursor) :
+    'Retrieve all equipment data'
+    mycursor.execute("SELECT * FROM equipment")
+    table = mycursor.fetchall()
+
+    return table
+
+def location(mycursor) :
+    'Retrieve all location data'
+    mycursor.execute("SELECT * FROM location")
+    table = mycursor.fetchall()
+
+    return table
+
+def manufacturer(mycursor) :
+    'Retrieve all manufacturer data'
+    mycursor.execute("SELECT * FROM manufacturer")
+    table = mycursor.fetchall()
+
+    return table
+
+def model(mycursor) :
+    'Retrieve all model data'
+    mycursor.execute("SELECT * FROM model")
+    table = mycursor.fetchall()
+
+    return table
+
+def service(mycursor) :
+    'Retrieve all service data'
+    mycursor.execute("SELECT * FROM service")
+    table = mycursor.fetchall()
+
+    return table
+
+
 # Retrive all data in the database
 def retrive_tables(mycursor) :
     AllTables = dict()
+    AllTables["admin"] = admin(mycursor)
+    AllTables["device"] = device(mycursor)
+    AllTables["equipment"] = equipment(mycursor)
+    AllTables["location"] = location(mycursor)
+    AllTables["manufacturer"] = manufacturer(mycursor)
+    AllTables["model"] = model(mycursor)
+    AllTables["service"] = service(mycursor)
+
     return AllTables
--- Server/main.py
+++ Server/main.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import json
 import database
-import re # REGEX
+import re # Regex
 #--------------------------------------------------------------------------#
 
 """Functions"""
@@ -18,35 +18,85 @@ db_tables = database.retrive_tables(mycursor)
 
 #--------------------------------------------------------------------------#
 
-"""Our Website"""
-website = Flask(__name__)
+"""Our app"""
+app = Flask(__name__)
+app.secret_key = 'hlzgzxpzlllkgzrn' # Your Secret_Key
 
 #--------------------------------------------------------------------------#
 
 """ Routes of Pages """
 # Home Page
-@website.route("/Dashboard")
-def Home():
-  
-  return render_template("index.html",
-                        title="Dashboard")
+@app.route("/")
+def home():
+  db_tables = database.retrive_tables(mycursor)
+
+  if 'loggedin' in session and session['loggedin'] == True:
+    return render_template("index.html",
+                       title="Dashboard")
+  else:
+    return redirect(url_for('login'))
+
 
 
 # Add New device
-@website.route("/AddDevice", methods=['GET', 'POST'])
-def AddDevice():
-  
-  return render_template("add.html",
-                        title="Add New Device")
+@app.route("/AddDevice", methods=['GET', 'POST'])
+def addDevice():
+  db_tables = database.retrive_tables(mycursor)
+  if 'loggedin' in session and session['loggedin'] == True:
+    return render_template("add.html",
+                          title="Add New Device")
+  else:
+    return redirect(url_for('login'))
 
 # Login
-@website.route("/", methods=['GET', 'POST'])
-def Login():
-  
-  return render_template("login.html",
-                        title="Login")
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  if 'loggedin' in session and session['loggedin'] == True:
+    return redirect(url_for('home'))
+  else:
+    status = True
+    if request.method == 'POST':
+      username  = request.form['username']
+      password  = request.form['password']
+
+      # Check if account exists using MySQL
+      mycursor.execute('SELECT * FROM admin WHERE `username` = %s AND `passwd` = %s', (username, password,))
+
+      # Fetch one record and return result
+      account = mycursor.fetchone()
+      print(username,password)
+      print(account)
+
+      # If account exists in accounts table in out database
+      if account:
+          # Create session data, we can access this data in other routes
+          session['loggedin'] = True
+          session['id'] = account[0]
+          session['username'] = account[1]
+          status = True
+          # Redirect to home page
+          return redirect(url_for('home'))
+      else:
+          # Account doesnt exist or username/password incorrect
+          status = False
+
+    return render_template("login.html",
+                          title="Login",
+                          status=status)
+
+# Logout
+@app.route("/logout")
+def logout():
+  # Remove session data, this will log the user out
+  session.pop('loggedin', None)
+  session.pop('id', None)
+  session.pop('username', None)
+  # Redirect to login page
+  return redirect(url_for('login'))
+
 
 
 # Run app
 if __name__ == "__main__":  
-  website.run(debug=True,port=9000)
+
+  app.run(debug=True,port=9000)
